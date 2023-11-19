3f27e19a540cfba0dab756e5e34e8bb584a90f66
Kentaro Nishioka <knishiok@c1r24s8.42tokyo.jp>
2023-11-19 21:36:29 +0900

    wrote header file

--- includes/ast.h
+++ includes/ast.h
@@ -1,13 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.c                                           :+:      :+:    :+:   */
+/*   ast.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: knishiok <knishiok@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/19 21:22:51 by knishiok          #+#    #+#             */
-/*   Updated: 2023/11/19 21:34:08 by knishiok         ###   ########.fr       */
+/*   Created: 2023/11/19 21:24:10 by knishiok          #+#    #+#             */
+/*   Updated: 2023/11/19 21:36:06 by knishiok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parser.h"
+#ifndef AST_H
+# define AST_H
+
+# include "type.h"
+
+typedef enum e_ast_type
+{
+	AST_COMMAND,
+	AST_AND,
+	AST_OR,
+	AST_PIPE,
+}	t_ast_type;
+
+typedef struct s_ast_node
+{
+	t_ast_type			type;
+	struct s_ast_node	*left;
+	struct s_ast_node	*right;
+	t_list				*redir;
+	t_list				*value;
+}	t_ast_node;
+
+typedef enum e_redir_type
+{
+	REDIR_IN,
+	REDIR_OUT,
+	REDIR_APPEND,
+	REDIR_HEREDOC,
+}	t_redir_type;
+
+#endif
