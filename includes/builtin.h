2e3c3ad2b7c74a9c705c21003a062737d921ca39
Mike Sargsyan <misargsy@c1r24s9.42tokyo.jp>
2023-11-19 16:25:37 +0900

    builtin: builtin.h

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,7 @@
 /*   By: misargsy <misargsy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 17:14:09 by misargsy          #+#    #+#             */
-/*   Updated: 2023/11/18 20:03:23 by misargsy         ###   ########.fr       */
+/*   Updated: 2023/11/19 16:13:54 by misargsy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,12 @@
 # include "libft.h"
 # include "type.h"
 # include <stdbool.h>
+# include <sys/errno.h>
+# include <string.h>
 
-int	bi_echo(t_token *token);
-int	bi_exit(t_token *token, bool parent);
+bool	can_go_next(t_token *token);
+int		bi_echo(t_token *token);
+int		bi_exit(t_token *token, bool parent);
+int		bi_pwd(void);
 
 #endif
